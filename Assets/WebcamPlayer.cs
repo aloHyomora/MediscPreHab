5a59458769a52ec09e0ea4892d483e4be89ba7f1
aloHyomora <alohomoratolit@naver.com>
2024-07-24 18:29:00 +0900

    [Feat] : 모든 카메라 해상도에 대응 1

--- Assets/WebcamPlayer.cs
+++ Assets/WebcamPlayer.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,7 +9,10 @@ public class WebcamPlayer : MonoBehaviour
 {
   public RawImage webcamRawImage;
   private WebCamTexture _webCamTexture;
+  public TextMeshProUGUI text;
 
+  public int webcamWidth;
+  public int webcamHeight;
   private void Awake()
   {
     InitWebcam();
@@ -16,26 +20,49 @@ public class WebcamPlayer : MonoBehaviour
 
   void InitWebcam()
   {
+    ResetWebCam();
+    
+    WebCamDevice device = WebCamTexture.devices[0];
+    _webCamTexture = new WebCamTexture(device.name);
+    
+    Debug.Log(webcamRawImage.texture.height);
+    Debug.Log(webcamRawImage.texture.width);
+    /*Debug.Log(  "[R]width "+_webCamTexture.requestedWidth+" hegith" + _webCamTexture.requestedHeight);
+    Debug.Log(  "width "+_webCamTexture.autoFocusPoint+" hegith " + _webCamTexture.mipmapCount);
+
+    Debug.Log(  _webCamTexture.texelSize);*/
+
+    //Debug.Log(  "width "+_webCamTexture. +" hegith " + _webCamTexture.height);
+    Debug.Log(  "width "+_webCamTexture.width+" hegith " + _webCamTexture.height);
+
+    webcamRawImage.texture = _webCamTexture;
+    
+    _webCamTexture.Play();
+    webcamWidth = _webCamTexture.width;
+    webcamHeight = _webCamTexture.height;
+
+    Debug.Log(  "[W]width "+_webCamTexture.width+" hegith " + _webCamTexture.height);
+    Debug.Log(  "[I]width "+ webcamRawImage.texture.width+" hegith " + webcamRawImage.texture.height);
+    
+    text.text = $"{webcamRawImage.texture.width}x{webcamRawImage.texture.height}";
+    ResetWebCam();
+    
+
+
+    RectTransform webcamRectTransform = webcamRawImage.GetComponent<RectTransform>();
+    webcamRectTransform.sizeDelta = new Vector2(webcamWidth, webcamHeight);  // .rect.width = new Rect(webcamRawImage.texture.width, webcamRawImage.texture.height);
+    Debug.Log(webcamRectTransform.sizeDelta);
+    // 1920x1080 -> 1920x1200
+    // 1944 * 1920 / 2592
+  }
+
+  private void ResetWebCam()
+  {
     if (_webCamTexture != null)
     {
       webcamRawImage.texture = null;
       _webCamTexture.Stop();
       _webCamTexture = null;
     }
-    WebCamDevice device = WebCamTexture.devices[0];
-    _webCamTexture = new WebCamTexture(device.name);
-    webcamRawImage.texture = _webCamTexture;
-    _webCamTexture.Play();
   }
-
-  IEnumerator PlayWebcam()
-  {
-    while (true)
-    {
-		    
-      yield return null;
-    }
-	    
-    yield return null;
-  } 
 }
